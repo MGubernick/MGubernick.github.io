760589db36fa7bad068f0e32dc954d5032539b14
MGubernick <MGubernick61@gmail.com>
2021-02-05 20:19:53 -0600

    Second commit
    - Portfolio now includes my name, yay!

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
+    * Start Bootstrap - Portfolio-MG v6.0.5 (https://startbootstrap.com/theme/freelancer)
     * Copyright 2013-2021 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+    * Licensed under MIT (https://github.com/StartBootstrap/Portfolio-MG/blob/master/LICENSE)
     */
     (function($) {
     "use strict"; // Start of use strict

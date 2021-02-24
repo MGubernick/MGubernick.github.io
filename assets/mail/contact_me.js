69fa85e851905e5bf3fa65e696cc8b4960ad5ab4
MGubernick <MGubernick61@gmail.com>
2021-02-24 14:44:12 -0600

    Try something different
    - Trying to use the jQuery already written into the template and use
    those to clear form

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -1,6 +1,6 @@
 $(function () {
     $(
-        "#contactForm input,#contactForm textarea,#contactForm button"
+        "#contactForm, #contactForm button"
     ).jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
@@ -9,27 +9,16 @@ $(function () {
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
-            var email = $("input#email").val();
-            var phone = $("input#phone").val();
-            var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            // var name = $("input#name").val();
+            // var email = $("input#email").val();
+            // var phone = $("input#phone").val();
+            // var message = $("textarea#message").val();
+            // var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(" ") >= 0) {
-                firstName = name.split(" ").slice(0, -1).join(" ");
-            }
+            // if (firstName.indexOf(" ") >= 0) {
+            //     firstName = name.split(" ").slice(0, -1).join(" ");
+            // }
             $this = $("#sendMessageButton");
-            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-            $.ajax({
-                url: "/assets/mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message,
-                },
-                cache: false,
                 success: function () {
                     // Success message
                     $("#success").html("<div class='alert alert-success'>");
@@ -63,11 +52,6 @@ $(function () {
                     $("#success > .alert-danger").append("</div>");
                     //clear all fields
                     $("#contactForm").trigger("reset");
-                },
-                complete: function () {
-                    setTimeout(function () {
-                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-                    }, 1000);
                 },
             });
         },

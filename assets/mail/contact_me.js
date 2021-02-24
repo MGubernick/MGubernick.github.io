2354e6d9501c2770e5126096dc9fee2ea0683641
MGubernick <MGubernick61@gmail.com>
2021-02-24 14:56:13 -0600

    Try This Again

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -2,22 +2,12 @@ $(function () {
     $(
         "#contactForm, #contactForm button"
     ).jqBootstrapValidation({
-        preventSubmit: true,
-        submitError: function ($form, event, errors) {
-            // additional error messages or events
-        },
+        // preventSubmit: true,
+        // submitError: function ($form, event, errors) {
+        //     // additional error messages or events
+        // },
         submitSuccess: function ($form, event) {
             event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
-            // var name = $("input#name").val();
-            // var email = $("input#email").val();
-            // var phone = $("input#phone").val();
-            // var message = $("textarea#message").val();
-            // var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            // if (firstName.indexOf(" ") >= 0) {
-            //     firstName = name.split(" ").slice(0, -1).join(" ");
-            // }
             $this = $("#sendMessageButton");
                 success: function () {
                     // Success message
@@ -54,9 +44,6 @@ $(function () {
                     $("#contactForm").trigger("reset");
                 },
             });
-        },
-        filter: function () {
-            return $(this).is(":visible");
         },
     });
 
